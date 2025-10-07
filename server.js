97cde8fe55781e87314969dceabab2f1e48f2887
Manav sagar <manavsagar46@gmail.com>
2025-10-07 22:09:44 +0530

    serverChange4

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ let isConnected = false;
 const connectDB = async () => {
   if (isConnected) return;
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     isConnected = true;
     console.log("MongoDB connected");
   } catch (err) {
