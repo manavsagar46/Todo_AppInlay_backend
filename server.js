233406b4ed12466cfcc181c52dac5a348947543b
Manav sagar <manavsagar46@gmail.com>
2025-10-07 20:26:03 +0530

    serverChanges

--- server.js
+++ server.js
@@ -11,12 +11,25 @@ app.use(cors());
 app.use(express.json());
 
 // db connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected ");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1); 
+  }
+};
+
+connectDB();
 
 app.use("/task", taskRoutes);
 
+app.get("/", (req, res) => {
+  res.send("🚀 Server is running and connected to MongoDB!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
